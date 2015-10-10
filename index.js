25c7dcdb16fe60a16c8428cdad35dfd2520ef5df
rossharper <ross@rossharper.net>
2015-10-10 16:18:29 +0100

    refactoring message sending

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function MessageSender(serialPort) {
     this.serialPort = serialPort;
 }
 
-MessageSender.prototype.sendMessage(message) {
+MessageSender.prototype.sendMessage = function(message) {
         serialPort.write(message, function(err, results) {
         if(err) {
             console.log('message sender serial port write err ' + err);
