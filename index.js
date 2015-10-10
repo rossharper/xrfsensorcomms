6baa2de728ad5b3c444ed4d5fe87208e5d990f00
rossharper <ross@rossharper.net>
2015-10-10 20:25:45 +0100

    upped interval back to 120s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport').SerialPort;
     SensorListener = require('./sensorlistener').SensorListener;
 
-var messageInterval = 30;
+var messageInterval = 120;
 var serialPort = new SerialPort("/dev/ttyAMA0", {
     baudrate: 9600
 });
