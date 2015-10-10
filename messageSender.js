81db3494b1ee813534aed4c34de447692fdc54d9
rossharper <ross@rossharper.net>
2015-10-10 16:44:56 +0100

    fix messagesender

--- messageSender.js
+++ messageSender.js
@@ -1,14 +1,12 @@
 function MessageSender(serialPort) {
-    this.serialPort = serialPort;
-}
-
-MessageSender.prototype.sendMessage = function(message) {
+    this.sendMessage = function(message) {
         this.serialPort.write(message, function(err, results) {
-        if(err) {
-            console.log('message sender serial port write err ' + err);
-        }
-        this.serialPort.drain();
-    });
+            if(err) {
+                console.log('message sender serial port write err ' + err);
+            }
+            this.serialPort.drain();
+        });
+    };
 }
 
 module.exports = {
