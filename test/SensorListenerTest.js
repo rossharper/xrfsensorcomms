cd5878341c5841898fb53d78c0f5bf1b1413c3ea
Ross Harper <ross.harper@bbc.co.uk>
2016-07-19 14:36:52 +0100

    Adding empty unit test suite

---
+++ test/SensorListenerTest.js
@@ -0,0 +1,3 @@
+// TODO: test sensor listener. Supplied with a serial port, config (message interval), repository, message parsers
+// test that for serial port data coming in, the repository gets the right messages
+// cover the listner and the message parsers.
