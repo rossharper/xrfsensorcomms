9552a8fd4ef3a8a4e8f2c240c29525a552d88ef0
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:46:49 +0100

    delint sensordatafile

--- SensorDataFile.js
+++ SensorDataFile.js
@@ -1,34 +1,36 @@
-var fs = require('fs');
+'use strict';
+
+const fs = require('fs');
 
 function writeSensorDataFile(sensorDataPath, dataFile, device, value) {
-    var devicePath = sensorDataPath + '/' + device;
-    ensureDeviceDirectoryExists(devicePath, function(err) {
-        if (err) {
-            console.log("Failed to create device directory: " + devicePath + " with err: " + err);
-        }
-        else {
-            writeDeviceDataValue(devicePath + "/" + dataFile, value)
-        }
-    });
+  // TODO: use path.join!
+  const devicePath = sensorDataPath + '/' + device;
+  ensureDeviceDirectoryExists(devicePath, (err) => {
+    if (err) {
+      console.log('Failed to create device directory: ' + devicePath + ' with err: ' + err);
+    } else {
+      writeDeviceDataValue(devicePath + '/' + dataFile, value);
+    }
+  });
 }
 
 function writeDeviceDataValue(filePath, value) {
-    fs.writeFile(filePath, value, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
+  fs.writeFile(filePath, value, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
 }
 
 function ensureDeviceDirectoryExists(path, callback) {
-    fs.mkdir(path, function(err) {
-        if (err) {
-            if (err.code == 'EEXIST') callback(null); // ignore the error if the folder already exists
-            else callback(err); // something else went wrong
-        } else callback(null); // successfully created folder
-    });
+  fs.mkdir(path, (err) => {
+    if (err) {
+      if (err.code === 'EEXIST') callback(null); // ignore the error if the folder already exists
+      else callback(err); // something else went wrong
+    } else callback(null); // successfully created folder
+  });
 }
 
 module.exports = {
-    writeSensorDataFile : writeSensorDataFile
-}
+  writeSensorDataFile: writeSensorDataFile
+};
