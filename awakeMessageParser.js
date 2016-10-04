3ba07ead47030077f48ccd6eb957c79c24720d36
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:18:26 +0100

    add eslint config & delint awakemessageparser

--- awakeMessageParser.js
+++ awakeMessageParser.js
@@ -1,13 +1,15 @@
+'use strict';
+
 function AwakeMessageParser(xrfParser, onAwakeCallback) {
-    this.parseMessage = function(message) {
-        if(message.indexOf("AWAKE") >= 0) {
-            onAwakeCallback(xrfParser.getDeviceNameFromMessage(message));
-            return true;
-        }
-        return false;
+  this.parseMessage = function (message) {
+    if (message.indexOf('AWAKE') >= 0) {
+      onAwakeCallback(xrfParser.getDeviceNameFromMessage(message));
+      return true;
     }
+    return false;
+  };
 }
 
 module.exports = {
-    AwakeMessageParser : AwakeMessageParser
-}
+  AwakeMessageParser: AwakeMessageParser
+};
