5e5bcd6f125996a4e2fa53457d835ecbf3f6520d
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:50:16 +0100

    delint temp message parser

--- temperatureMessageParser.js
+++ temperatureMessageParser.js
@@ -1,16 +1,18 @@
+'use strict';
+
 function TemperatureMessageParser(xrfParser, onTempCallback) {
-    this.parseMessage = function(message) {
-        var payload = message.match(/a[A-Z][A-Z]TMPA(-?[0-9\.]{4,5})/);
-        if(payload) {
-            onTempCallback(
-                xrfParser.getDeviceNameFromMessage(message),
-                payload[1]);
-            return true;
-        }
-        return false;
+  this.parseMessage = function (message) {
+    const payload = message.match(/a[A-Z][A-Z]TMPA(-?[0-9\.]{4,5})/);
+    if (payload) {
+      onTempCallback(
+        xrfParser.getDeviceNameFromMessage(message),
+        payload[1]);
+      return true;
     }
+    return false;
+  };
 }
 
 module.exports = {
-    TemperatureMessageParser : TemperatureMessageParser
-}
+  TemperatureMessageParser: TemperatureMessageParser
+};
