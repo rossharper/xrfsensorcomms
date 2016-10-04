efab0e5c347040ab1ff6e15bc909c0417ba9a010
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:22:31 +0100

    delint data repo factory

--- dataRepositoryFactory.js
+++ dataRepositoryFactory.js
@@ -1,15 +1,17 @@
-var BatteryDataRepository = require('./FileSystemBatteryDataRepository').BatteryDataRepository,
-    BatteryLowDataRepository = require('./FileSystemBatteryLowDataRepository').BatteryLowDataRepository,
-    TemperatureDataRepository = require('./FileSystemTemperatureDataRepository').TemperatureDataRepository;
+'use strict';
+
+const BatteryDataRepository = require('./FileSystemBatteryDataRepository').BatteryDataRepository;
+const BatteryLowDataRepository = require('./FileSystemBatteryLowDataRepository').BatteryLowDataRepository;
+const TemperatureDataRepository = require('./FileSystemTemperatureDataRepository').TemperatureDataRepository;
 
 module.exports = {
-    createBatteryDataRepository : function(sensorDataPath) {
-        return new BatteryDataRepository(sensorDataPath);
-    },
-    createTemperatureDataRepository : function(sensorDataPath) {
-        return new TemperatureDataRepository(sensorDataPath);
-    },
-    createBatteryLowDataRepository : function(sensorDataPath) {
-        return new BatteryLowDataRepository(sensorDataPath);
-    }
-}
+  createBatteryDataRepository: function (sensorDataPath) {
+    return new BatteryDataRepository(sensorDataPath);
+  },
+  createTemperatureDataRepository: function (sensorDataPath) {
+    return new TemperatureDataRepository(sensorDataPath);
+  },
+  createBatteryLowDataRepository: function (sensorDataPath) {
+    return new BatteryLowDataRepository(sensorDataPath);
+  }
+};
