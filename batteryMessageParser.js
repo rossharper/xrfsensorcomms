1f7643fedfde748eecf9406422d75a3d3d5d3031
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:21:46 +0100

    delint batt level parser

--- batteryMessageParser.js
+++ batteryMessageParser.js
@@ -1,16 +1,18 @@
+'use strict';
+
 function BatteryMessageParser(xrfParser, onBattCallback) {
-    this.parseMessage = function(message) {
-        var payload = message.match(/a[A-Z][A-Z]BATT(-?[0-9\.]{4,5})/);
-        if(payload) {
-            onBattCallback(
-                xrfParser.getDeviceNameFromMessage(message),
-                payload[1]);
-            return true;
-        }
-        return false;
+  this.parseMessage = function (message) {
+    const payload = message.match(/a[A-Z][A-Z]BATT(-?[0-9\.]{4,5})/);
+    if (payload) {
+      onBattCallback(
+        xrfParser.getDeviceNameFromMessage(message),
+        payload[1]);
+      return true;
     }
+    return false;
+  };
 }
 
 module.exports = {
-    BatteryMessageParser : BatteryMessageParser
-}
+  BatteryMessageParser: BatteryMessageParser
+};
