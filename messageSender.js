369e1a9b90dada5e29d1390a5d4820aaa444b2c5
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:38:56 +0100

    delint MessageSender

--- messageSender.js
+++ messageSender.js
@@ -1,15 +1,17 @@
+'use strict';
+
 function MessageSender(serialPort) {
-    this.sendMessage = function(message) {
-        console.log("sending message: " + message);
-        serialPort.write(message, function(err, results) {
-            if(err) {
-                console.log('message sender serial port write err ' + err);
-            }
-            serialPort.drain();
-        });
-    };
+  this.sendMessage = function (message) {
+    console.log('sending message: ' + message);
+    serialPort.write(message, (err) => {
+      if (err) {
+        console.log('message sender serial port write err ' + err);
+      }
+      serialPort.drain();
+    });
+  };
 }
 
 module.exports = {
-    MessageSender : MessageSender
-}
+  MessageSender: MessageSender
+};
