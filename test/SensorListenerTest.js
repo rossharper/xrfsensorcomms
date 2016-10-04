abc391046ea1ce60b73c2e7687a968e6d68f5cde
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 14:34:35 +0100

    add a test description

--- test/SensorListenerTest.js
+++ test/SensorListenerTest.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // TODO: test sensor listener. Supplied with a serial port, config (message interval), repository, message parsers
 // test that for serial port data coming in, the repository gets the right messages
 // cover the listner and the message parsers.
@@ -14,13 +16,12 @@
 // pattern appears to be:
 // /a([A-Z]{2})([A-Z]{4})([A-Z0-9\.-]{5})/
 
-var chai = require('chai');
-var expect = chai.expect;
-var spies = require('chai-spies');
-
-
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require('chai-spies');
 
 describe("XRF Sensor Listener", function() {
-  it("should do something", function() {
-  });
+    it("should open the serial port", function() {
+
+    });
 });
