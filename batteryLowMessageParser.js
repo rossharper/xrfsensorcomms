8c87ca240e2c31044130acab486da474e6c50b23
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:20:55 +0100

    delint batt low parser

--- batteryLowMessageParser.js
+++ batteryLowMessageParser.js
@@ -1,14 +1,16 @@
+'use strict';
+
 function BatteryLowMessageParser(xrfParser, onBattLowCallback) {
-    this.parseMessage = function(message) {
-        var payload = message.match(/a[A-Z][A-Z]BATTLOW--/);
-        if(payload) {
-            onBattCallback(xrfParser.getDeviceNameFromMessage(message));
-            return true;
-        }
-        return false;
+  this.parseMessage = function (message) {
+    const payload = message.match(/a[A-Z][A-Z]BATTLOW--/);
+    if (payload) {
+      onBattLowCallback(xrfParser.getDeviceNameFromMessage(message));
+      return true;
     }
+    return false;
+  };
 }
 
 module.exports = {
-    BatteryLowMessageParser : BatteryLowMessageParser
-}
+  BatteryLowMessageParser: BatteryLowMessageParser
+};
