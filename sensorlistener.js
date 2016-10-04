5e8d7dae2e5ece54dd33ec065197038ca20c9a82
Ross Harper <ross.harper@bbc.co.uk>
2016-10-04 16:49:41 +0100

    delint SensorListener

--- sensorlistener.js
+++ sensorlistener.js
@@ -1,54 +1,56 @@
-var MessageSender = require('./messageSender').MessageSender,
-    IntervalUpdater = require('./intervalUpdater').IntervalUpdater,
-    messageParserFactory = require('./messageParserFactory')
+'use strict';
 
-var messageLength = 12;
+const MessageSender = require('./messageSender').MessageSender;
+const IntervalUpdater = require('./intervalUpdater').IntervalUpdater;
+const messageParserFactory = require('./messageParserFactory');
+
+const messageLength = 12;
 
 function SensorListener(serialPort, messageInterval, sensorDataPath) {
-    var buffer = "";
-    var messageParsers = messageParserFactory.createMessageParsers(
-        new IntervalUpdater(new MessageSender(serialPort), messageInterval),
-        sensorDataPath);
+  let buffer = '';
+  const messageParsers = messageParserFactory.createMessageParsers(
+    new IntervalUpdater(new MessageSender(serialPort), messageInterval),
+    sensorDataPath);
 
-    var parseMessage = function(message) {
-        if(message[0] != 'a') return;
+  function parseMessage(message) {
+    if (message[0] !== 'a') return;
 
-        for (var i = 0; i < messageParsers.length; i++) {
-            parsed = messageParsers[i].parseMessage(message);
-            if(parsed) break;
-        };
+    for (let i = 0; i < messageParsers.length; i++) {
+      const parsed = messageParsers[i].parseMessage(message);
+      if (parsed) break;
     }
+  }
 
-    var processBuffer = function() {
-        while(buffer.length >= messageLength) {
-            var message = buffer.substr(0, messageLength);
-            //console.log('PROCESS message: ' + message);
-            buffer = buffer.substr(messageLength);
-            //console.log('POST-PROCESS buffer: ' + buffer);
-            parseMessage(message);
-        }
+  function processBuffer() {
+    while (buffer.length >= messageLength) {
+      const message = buffer.substr(0, messageLength);
+      //console.log('PROCESS message: ' + message);
+      buffer = buffer.substr(messageLength);
+      //console.log('POST-PROCESS buffer: ' + buffer);
+      parseMessage(message);
     }
+  }
 
-    this.listen = function() {
-        serialPort.open(function (error) {
-            if ( error ) {
-                console.log('failed to open: '+error);
-            } else {
-                console.log('open');
-                serialPort.on('data', function(data) {
+  this.listen = function () {
+    serialPort.open((error) => {
+      if (error) {
+        console.log('failed to open: ' + error);
+      } else {
+        console.log('open');
+        serialPort.on('data', (data) => {
 
-                    console.log('data received: ' + data);
+          console.log('data received: ' + data);
 
-                    buffer += data;
-                    //console.log('buffer after data receive: ' + buffer);
+          buffer += data;
+          //console.log('buffer after data receive: ' + buffer);
 
-                    processBuffer();
-                });
-            }
+          processBuffer();
         });
-    }
+      }
+    });
+  };
 }
 
 module.exports = {
-    SensorListener : SensorListener
-}
+  SensorListener: SensorListener
+};
