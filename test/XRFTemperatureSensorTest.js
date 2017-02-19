6a8db94ed3b306b4a8f9ed2a9d35e913f720eb72
Ross Harper <ross@rossharper.net>
2017-02-19 18:49:25 +0000

    Reenable dodgy tests for safe refactoring
    Tests have waits in them to allow IO to finish before asserting.
    Allows refactoring safely until test seams can be put in the correct places.

--- test/XRFTemperatureSensorTest.js
+++ test/XRFTemperatureSensorTest.js
@@ -7,12 +7,15 @@ const rimraf = require('rimraf');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe.skip('temporary end-to-end refactoring tests', () => {
+describe('temporary end-to-end refactoring tests', () => {
 
-  // these tests fail, because the serial port call with data is synchronous, but
+  // these tests have dodgy waits in,
+  // because the serial port call with data is synchronous, but
   // the file writing is async fire and forget
   // making file writing may make the serial processing too slow (for the awake interval message to fire in time)
   // setImmediate doesn't make the test wait long enough for the test to pass
+  // so for now, to enable safe refactoring, there are dodgy waits
+  // once appropriate test seams are in place, these waits can be removed.
 
   const SENSOR_PATH = './testsensordata';
 
@@ -73,41 +76,41 @@ describe.skip('temporary end-to-end refactoring tests', () => {
   it('should save a temperature when message received', (done) => {
     onDataCb('aXATMPA20.18');
 
-    setImmediate(() => {
+    setTimeout(() => {
       const temperatureValue = readTemperatureValue('XA');
       expect(temperatureValue).to.equal('20.18');
       done();
-    });
+    }, 500);
   });
 
   it('should save a battery level when message received', (done) => {
     onDataCb('aXBBATT2.78-');
 
-    setImmediate(() => {
+    setTimeout(() => {
       const temperatureValue = readBatteryValue('XB');
       expect(temperatureValue).to.equal('2.78');
       done();
-    });
+    }, 500);
   });
 
   it('should save a battery low flag when message received', (done) => {
     onDataCb('aXCBATTLOW--');
 
-    setImmediate(() => {
+    setTimeout(() => {
       const battLowValue = readBatteryLowValue('XC');
       expect(battLowValue).to.equal('1');
       done();
-    });
+    }, 500);
   });
 
   it('should save value when message split across serial port callbacks', (done) => {
     onDataCb('aXDTMPA2');
     onDataCb('1.12');
 
-    setImmediate(() => {
+    setTimeout(() => {
       const temperatureValue = readTemperatureValue('XD');
       expect(temperatureValue).to.equal('21.12');
       done();
-    });
+    }, 500);
   });
 });
