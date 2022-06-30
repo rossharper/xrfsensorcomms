3df60b793aede80599774956b4e786b0bd8961dd
rossharper <rossharper@users.noreply.github.com>
2022-06-30 18:24:07 +0100

    Fix: humidity callback

--- humidityMessageParser.js
+++ humidityMessageParser.js
@@ -4,7 +4,7 @@ function HumidityMessageParser(xrfParser, onHumidityCallback) {
   this.parseMessage = function (message) {
     const payload = message.match(/a[A-Z0-9][A-Z0-9]HUM(-?[0-9\.]{4,5})/);
     if (payload) {
-      onTempCallback(
+      onHumidityCallback(
         xrfParser.getDeviceNameFromMessage(message),
         payload[1]);
       return true;
